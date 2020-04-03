e016a781dbed9110f762ee28e523451a406a5c58
Mark Hutter <mrkhutter@gmail.com>
2020-04-02 20:33:44 -0500

    good first pass on a base level profile page so that one can exist on the interwebs

---
+++ data.js
@@ -0,0 +1,32 @@
+const main = {
+  name: 'Mark Hutter',
+  mail: 'mrkhutter@gmail.com',
+  img: 'profile.jpg',
+  role: ['Lead Engineer', ' Conference Organizer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/mrkhutter'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/mark-hutter-9446352b/'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/mrkhutter'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'mrkhutter@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Birmingham on Rails', link: 'https://www.birminghamonrails.com' }
+  ]
+};
